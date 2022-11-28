5921e47892191afb50946fa836abceac25080679
oliviermeslin <olivier.meslin@insee.fr>
2022-11-28 23:16:34 +0000

    Commit

--- script.R
+++ script.R
@@ -83,7 +83,23 @@ df %>%
   ), alpha = 0.2) +
   geom_histogram() # position = "dodge") + scale_fill_viridis_d()
 
+stats_age <- df %>%
+  group_by(decennie = decennie_a_partir_annee(age)) %>%
+  summarise(n())
 
+table_age <- gt::gt(stats_age) %>%
+  gt::tab_header(
+    title = "Distribution des âges dans notre population"
+  ) %>%
+  gt::fmt_number(
+    columns = `n()`,
+    sep_mark = " ",
+    decimals = 0
+  ) %>%
+  gt::cols_label(
+    decennie = "Tranche d'âge",
+    `n()` = "Population"
+  )
 
 ## Part d'homme dans chaque cohorte ===========
 ggplot(df %>%
